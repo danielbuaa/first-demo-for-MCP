a7cfc9e876a6c6b1b7dfa31876d7f8c12f74bb9c
buaadaniel <895004881@qq.com>
2025-04-27 16:24:18 +0800

    添加UserRepository接口

---
+++ src/main/java/com/example/firstdemo/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.firstdemo.dao;
+
+import com.example.firstdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 可以添加自定义查询方法
+}
