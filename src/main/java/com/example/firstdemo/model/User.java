8e665db4dbf3d852d80431e4b42cdb8f945a9848
buaadaniel <895004881@qq.com>
2025-04-28 11:06:47 +0800

    Add JavaDoc to User model

--- src/main/java/com/example/firstdemo/model/User.java
+++ src/main/java/com/example/firstdemo/model/User.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Entity class representing a user in the system
+ * 
+ * @author Daniel
+ * @since 2025-04-28
+ */
 @Entity
 @Table(name = "users")
 @Data
