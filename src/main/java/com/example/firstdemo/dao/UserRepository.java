6c9ddf6da2e47b23a2872d6595d0a66d280318fb
buaadaniel <895004881@qq.com>
2025-04-28 11:01:53 +0800

    Add javadoc to UserRepository

--- src/main/java/com/example/firstdemo/dao/UserRepository.java
+++ src/main/java/com/example/firstdemo/dao/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.firstdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Daniel
+ * @since 2025-04-28
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // 可以添加自定义查询方法
